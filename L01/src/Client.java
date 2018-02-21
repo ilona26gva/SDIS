efc1665ffcb578be35078587d3c332589cbf8932
Vitor Magalhães <up201503447@fe.up.pt>
2018-02-21 12:21:12 +0000

    Client Server works without while

--- L01/src/Client.java
+++ L01/src/Client.java
@@ -4,52 +4,57 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.util.Vector;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Client {
 
-	String host;
-	int port;
-	String oper;
-	Vector<String> args;	
+	public static void main(String[] args) throws IOException {
 
-	Client(String host, int port, String oper, Vector<String> args) throws IOException{
+		String host = args[0];
+		int port = Integer.parseInt(args[1]);
+		String oper = args[2];
 
 		String message = oper + " ";
-		
-		String thisisatest = "";
-		
 
-		System.out.println(thisisatest);
-		
-		if (oper == "register") {
-			if (args.size() == 2) {
-				message += args.get(0) + " ";
-				message += args.get(1).substring(0, 256);
-			}
-			else
-				System.out.println("Error on register");
-		}
-		else if (oper == "lookup"){
-			if (args.size() == 1)
-				message += args.get(0);
-			else
-				System.out.println("Error on Lookup");
+		System.out.println(oper);
 
+		if (oper.equals("register")) {
+			message += args[3] + " ";
+			message += args[4];
+			/*else
+				System.out.println("Error on register");*/
 		}
-		
+		else if (oper.equals("lookup")){
+			message += args[3];
+			/*	else
+				System.out.println("Error on Lookup");*/
+		}
+
 		byte[] buf = message.getBytes();
-		DatagramSocket s = new DatagramSocket(port);
+		DatagramSocket s = new DatagramSocket();
 		DatagramPacket p = new DatagramPacket(buf, buf.length, InetAddress.getByName(host), port);
-		
-		s.send(p);
-		
-		//waits for answer
-		s.receive(p);
-		
-		String response = p.getData().toString();
-		
-		System.out.println(response);		
-		
-	}
 
+		s.send(p);
+
+		s.close();
+		
+		s = new DatagramSocket(port);
+
+		//waits for answer
+		byte[] bufr = new byte[256]; 
+		
+		p = new DatagramPacket(bufr, bufr.length);		
+
+		s.receive(p);
+
+		String response = new String(p.getData());
+
+		System.out.println(response);	
+		
+		s.close();
+
+	}
 }
+
+
--- L01/src/Server.java
+++ L01/src/Server.java
@@ -6,28 +6,43 @@ import java.net.DatagramPacket;
 import java.util.Hashtable;
 
 public class Server {
+	
+	public static void main(String[] args) throws IOException {		
 
-	Hashtable<String, String> table;
+		Hashtable<String, String> table = new Hashtable<String,String>();
 
-	Server(int port) throws IOException{
+		table.put("24-21-JL", "Quintino");
+		table.put("53-56-LK", "Washington");
+		table.put("24-65-JF", "Edward");
 
+		int port = Integer.parseInt(args[0]);
+		
+		byte[] buf = new byte[256];
+		
 		DatagramSocket s = new DatagramSocket(port);
-		DatagramPacket p = null;		
+		DatagramPacket p = new DatagramPacket(buf, buf.length);		
 		
 		s.receive(p);
+		
+		s.close();
+		
+		s = new DatagramSocket();
 		
 		InetAddress ip = p.getAddress();
 
 		byte[] data = p.getData();
 
 		String request = new String(data);
-		String splited[] = request.split(" ");
+		
+		System.out.println("Request: " + request);
+		
+		String splited[] = request.trim().split(" ");
 		String oper = splited[0];		
 		String owner = table.get(splited[1]);
 
 		String result = "Invalid operation!";
 
-		if (oper == "register") {
+		if (oper.equals("register")) {
 			if (owner == null) {	
 				table.put(splited[1], splited[2]);
 				result = "Register successful!";
@@ -35,7 +50,7 @@ public class Server {
 			else
 				result = "-1";
 		}
-		else if (oper == "lookup") {
+		else if (oper.equals("lookup")){
 			if (owner == null)
 				result = "That plate does not exist!";
 			else
@@ -44,7 +59,10 @@ public class Server {
 		
 		byte[] response = result.getBytes();
 		
+		System.out.println(result);
+		
 		p = new DatagramPacket(response, response.length, ip, port);
+		
 		s.send(p);
 		
 		//infinite loop
