fc73f1cba33e94d67a9d244367fd6bd76c3cf389
bruno02960 <briguel1997>
2018-03-05 19:17:49 +0000

    Server main method added

--- Project 1/src/dbs/Peer.java
+++ Project 1/src/dbs/Peer.java
@@ -1,5 +1,20 @@
 package dbs;
 
 public class Peer {
-	int id; //How to make it unique?
+	static int id; //How to make it unique?
+	
+	public static void main(String[] args)
+	{
+		String protocolVersion = args[0];
+		String APaddr = args[2];						//AP stands for Access Point
+		int APport = Integer.parseInt(args[3]);
+		String MCaddr = args[4];
+		int MCport = Integer.parseInt(args[5]);
+		String MDBaddr = args[6];
+		int MDBport = Integer.parseInt(args[7]);
+		String MDRaddr = args[8];
+		int MDRport = Integer.parseInt(args[9]);
+		
+		id = Integer.parseInt(args[1]);
+	}
 }
